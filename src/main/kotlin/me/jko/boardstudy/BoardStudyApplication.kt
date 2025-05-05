fad90ef231ebc426819895bd7ffff4c185d6dd8d
junhee-ko <junheee.ko@gmail.com>
2025-05-05 17:23:51 +0900

    first commit

---
+++ src/main/kotlin/me/jko/boardstudy/BoardStudyApplication.kt
@@ -0,0 +1,11 @@
+package me.jko.boardstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardStudyApplication>(*args)
+}
---
+++ src/test/kotlin/me/jko/boardstudy/BoardStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jko.boardstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
