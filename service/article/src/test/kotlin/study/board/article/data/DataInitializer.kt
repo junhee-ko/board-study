9b7b75762fc96409f8eb2f846691c6f32f50a5e0
junhee-ko <junheee.ko@gmail.com>
2025-05-06 14:53:26 +0900

    init articles data into mysql table

---
+++ service/article/src/test/kotlin/study/board/article/data/DataInitializer.kt
@@ -0,0 +1,76 @@
+package study.board.article.data
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+import org.springframework.transaction.support.TransactionTemplate
+import snowflake.Snowflake
+import study.board.article.entity.Article
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@SpringBootTest
+@TestPropertySource(properties = ["spring.jpa.show-sql=false"])
+class DataInitializer {
+
+    @PersistenceContext
+    private lateinit var entityManager: EntityManager
+
+    @Autowired
+    private lateinit var transactionTemplate: TransactionTemplate
+
+    private lateinit var snowflake: Snowflake
+    private lateinit var countDownLatch: CountDownLatch
+    private lateinit var executorService: ExecutorService
+
+    @BeforeEach
+    fun setUp() {
+        snowflake = Snowflake()
+        countDownLatch = CountDownLatch(EXECUTE_COUNT)
+        executorService = Executors.newFixedThreadPool(30)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        executorService.shutdown()
+    }
+
+    @Test
+    fun init() {
+        repeat(EXECUTE_COUNT) {
+            executorService.submit {
+                insert()
+                countDownLatch.countDown()
+                println("remaining count: ${countDownLatch.count}")
+            }
+        }
+
+        countDownLatch.await()
+    }
+
+    private fun insert() {
+        transactionTemplate.executeWithoutResult {
+            repeat(BULK_INSERT_SIZE) { i ->
+                val article = Article.create(
+                    articleId = snowflake.nextId(),
+                    title = "title $i",
+                    content = "content $i",
+                    boardId = 1L,
+                    writerId = 1L
+                )
+                entityManager.persist(article)
+            }
+        }
+    }
+
+    companion object {
+        private const val BULK_INSERT_SIZE = 2000
+        private const val EXECUTE_COUNT = 6000
+    }
+}
